5521a3e5e650802ef1b02de858415c05d77c5584
dsw50231 <138249648+dsw50231@users.noreply.github.com>
2023-07-01 02:01:29 +0200

    added logs30

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,14 @@
 case $1 in
     "--date") date;;
     "--logs")
-        for i in {1..100}
+        if [ -n $2 ]
+        then
+            n=$2
+        else
+            n=100
+        fi
+
+        for ((i=1; i<=n; i++))
         do
             echo "log$i.txt skrypt.sh $(date)" > "log$i.txt"
         done
